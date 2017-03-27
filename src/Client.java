7518895aabeb19edf02f19750c005af50e8d50a2
josaphat mayuba <mayubandele@live.fr>
2017-03-27 17:20:10 -0400

    creation chat
    pas encore fini

--- src/Client.java
+++ src/Client.java
@@ -35,10 +35,10 @@ public class Client {
 	private Grille[] board = new Grille[9];
 	private Grille currentGrille;
 
-	private static int PORT = 8901;
-	private Socket socket;
-	private BufferedReader in;
-	private PrintWriter out;
+	private static int PORT= 8901,PORTCHAT=8902 ;
+	private Socket socket,socketchat;
+	private BufferedReader in,inchat;
+	private PrintWriter out,outchat;
 	private final JPanel panel = new JPanel();
 	private JTextArea textArea;
 	private JTextField editorPane;
@@ -54,7 +54,9 @@ public class Client {
 		socket = new Socket(serverAddress, PORT);
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		out = new PrintWriter(socket.getOutputStream(), true);
-
+		socketchat = new Socket(serverAddress, PORTCHAT);
+		inchat = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		outchat = new PrintWriter(socket.getOutputStream(), true);
 		// Layout GUI
 		JPanel boardPanel = new JPanel();
 		boardPanel.setBounds(59, 11, 389, 355);
@@ -98,7 +100,7 @@ public class Client {
 			public void keyPressed(KeyEvent arg0) {
 				if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
 
-					out.println("CHAT" + editorPane.getText());
+					outchat.println("CHAT" + editorPane.getText());
 					editorPane.setText("");
 				}
 
--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,22 @@
 import java.net.ServerSocket;
 
 public class Server {
-
+    private static final int PORT = 9001;
     public static void main(String[] args) throws Exception {
         ServerSocket listener = new ServerSocket(8901);
         System.out.println("Tic Tac Toe Server is Running");
+        System.out.println("The chat server is running.");
+            ServerSocket listenerchat = new ServerSocket(PORT);
         try {
             while (true) {
                 AppGame Game = new AppGame();
                 Game.runplayer(listener);
+                new Handler(listenerchat.accept()).start();
             }
+            
+          
         } finally {
-            listener.close();
+            listener.close();  listenerchat.close();
         }
     }
     
