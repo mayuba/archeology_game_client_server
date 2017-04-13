fe650eb97e61e30e7e8f732c21bcc320ada35e79
josaphat mayuba <mayubandele@live.fr>
2017-04-13 04:09:02 -0400

    2.3.1

--- src/Server.java
+++ src/Server.java
@@ -1,19 +1,43 @@
 import java.net.InetAddress;
 import java.net.ServerSocket;
+
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 
 public class Server {
+	static JFrame frame = new JFrame();
 
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	public static void main(String[] args) throws Exception {
 		String numPort = JOptionPane.showInputDialog(null, "Veuillez entrer le numero de port ", "Connecter le Serveur",
 				JOptionPane.QUESTION_MESSAGE);
 		ServerSocket listener = new ServerSocket(Integer.parseInt(numPort));
+		frame.getContentPane().setLayout(null);
+		InetAddress addr = InetAddress.getLocalHost();
+		System.out.println(addr.getHostName());
+		System.out.println(addr.getHostAddress());
+		JLabel lblAdresseIp = new JLabel("Adresse IP");
+		lblAdresseIp.setBounds(38, 31, 81, 14);
+		frame.getContentPane().add(lblAdresseIp);
 
+		JLabel lblNewLabelIP = new JLabel(addr.getHostAddress());
+		lblNewLabelIP.setBounds(143, 31, 168, 14);
+		frame.getContentPane().add(lblNewLabelIP);
+
+		JLabel lblPort = new JLabel("Port");
+		lblPort.setBounds(38, 76, 46, 14);
+		frame.getContentPane().add(lblPort);
+
+		JLabel lblNewLabel_port = new JLabel(numPort);
+		lblNewLabel_port.setBounds(143, 76, 168, 14);
+		frame.getContentPane().add(lblNewLabel_port);
+		frame.setVisible(true);
 		try {
 			while (true) {
-				InetAddress addr = InetAddress.getLocalHost();
-				System.out.println(addr.getHostName());
-				System.out.println(addr.getHostAddress());
+
 				AppGame Game = new AppGame();
 				Game.runplayer(listener);
 
@@ -22,5 +46,4 @@ public class Server {
 			listener.close();
 		}
 	}
-
 }
